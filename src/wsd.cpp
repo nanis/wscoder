f9ffd5994c4e4141eff07553604d0d2af4bd91c8
A. Sinan Unur <sinan@unur.com>
2021-10-22 10:36:34 -0400

    Factor out common code to main, convert to Franken C++

--- src/wsd.cpp
+++ src/wsd.cpp
@@ -2,32 +2,12 @@
 #include <cstdio>
 #include <cstdlib>
 #include <thread>
-
-#ifdef _WIN32
-#include <fcntl.h>
-#include <io.h>
-#endif
+#include <vector>
 
 #include "ws.hpp"
 
-// We want a function that maps:
-
-//              x |  y
-// -------------------
-//  9 (0000_1001) | 0
-// 10 (0000_1010) | 1
-// 13 (0000_1101) | 2
-// 32 (0010_0000) | 3
-
-// In integer arithmetic, x/10 gets us there but we have to pad the result when
-// x == 13. Division is costly, so instead of dividing by 10, we multiply by
-// ceil(64/10) = 7 and look at how many 64s are in the result. Using 7 (instead
-// of, e.g., 13, 26, or 0x1999999a) means we can do it all with uint8_t values
-// with no up/downcasting. Note 13 is the only x value for which !!(ws & 4) is
-// 1.
-
 static uint8_t
-ws_to_half_nibble(uint8_t ws)
+ws_to_half_nibble(const uint8_t& ws)
 {
     return ((7 * ws) >> 6) + !!(ws & 4);
 }
@@ -39,67 +19,43 @@ ws_decode(const uint8_t* buf)
            (ws_to_half_nibble(buf[2]) << 2) | ws_to_half_nibble(buf[3]);
 }
 
-static void*
-alloc_with_fail(size_t sz, const char* purpose)
-{
-    void* p = malloc(sz);
-    if (!p) {
-        fprintf(
-          stderr, "Failed to allocated '%zu' bytes for %s\n", sz, purpose);
-        perror("Malloc failed");
-        exit(EXIT_FAILURE);
-    }
-
-    return p;
-}
-
 static void
 decode_buffer_interleaved(const uint8_t* const bufin,
                           uint8_t* bufout,
-                          const size_t limit)
+                          const size_t limit,
+                          size_t num_threads)
 {
-    for (size_t i = 0; i < limit; i += 4 * NUM_THREADS) {
+    for (size_t i = 0; i < limit; i += 4 * num_threads) {
         bufout[i / 4] = ws_decode(bufin + i);
     }
 }
 
-static void
-decode_stream(FILE* fin, FILE* fout)
+void
+decode_stream(std::FILE* in, std::FILE* out, size_t num_threads)
 {
-    uint8_t* bufin = (uint8_t*)alloc_with_fail(ENCODED_BUFSIZE, "input buffer");
-    uint8_t* bufout =
-      (uint8_t*)alloc_with_fail(DECODED_BUFSIZE, "output buffer");
+    uint8_t* bufin = new uint8_t[ENCODED_BUFSIZE];
+    uint8_t* bufout = new uint8_t[DECODED_BUFSIZE];
 
-    std::thread decoders[NUM_THREADS];
-
+    std::vector<std::thread> decoders(num_threads);
     size_t n;
 
-    while ((n = fread(bufin, 1, ENCODED_BUFSIZE, fin))) {
-        for (size_t i = 0; i < NUM_THREADS; ++i) {
-            decoders[i] = std::thread(
-              decode_buffer_interleaved, bufin + 4 * i, bufout + i, n);
+    while ((n = std::fread(bufin, 1, ENCODED_BUFSIZE, in))) {
+        for (size_t i = 0; i < num_threads; ++i) {
+            decoders[i] = std::thread(decode_buffer_interleaved,
+                                      bufin + 4 * i,
+                                      bufout + i,
+                                      n,
+                                      num_threads);
         }
 
         for (auto& decoder : decoders) {
             decoder.join();
         }
 
-        if (fout) {
-            fwrite(bufout, 1, n / 4, fout);
-        }
+        std::fwrite(bufout, 1, n / 4, out);
     }
 
-    if (fout) {
-        fflush(fout);
-    }
-}
-
-int
-main(void)
-{
-#ifdef _WIN32
-    _setmode(fileno(stdin), O_BINARY);
-    _setmode(fileno(stdout), O_BINARY);
-#endif
-    decode_stream(stdin, stdout);
+    std::fflush(out);
+    delete[] bufout;
+    delete[] bufin;
 }
--- src/wse.cpp
+++ src/wse.cpp
@@ -2,35 +2,20 @@
 #include <cstdio>
 #include <cstdlib>
 #include <thread>
-
-#ifdef _WIN32
-#include <fcntl.h>
-#include <io.h>
-#endif
+#include <vector>
 
 #include "ws.hpp"
 
-// We want a function that maps:
-
-//      x |   y
-// --------------
-// 0 (00) |  9
-// 1 (01) | 10
-// 2 (10) | 13
-// 3 (11) | 32
-
-// Given 0 -> 9, we need an intercept term. 0, 1, and 2 are easily mapped to 9
-// + 0² = 9, 9 + 1² = 10, and 9 + 2² = 13, respectively. But, 3 maps to 9 + 3²
-// = 18, which means we need to add 14 only when x == 3. We'd like to do this
-// without using a conditional. Since bits 0 and 1 are both set only when x ==
-// 3, (c & (c >> 1)) only evaluates to 1 in that case and gives us a way to map
-// 3 to 32.
-
-// Caller is responsible for ensuring 0 <= c <= 3
 static uint8_t
-half_nibble_to_ws(uint8_t c)
+half_nibble_to_ws(const uint8_t& c)
 {
-    return (c * c) + 14 * (c & (c >> 1)) + BASE_CHR;
+    static const uint32_t x = (' ' << 24) |
+        ('\r' << 16) |
+        ('\n' << 8) |
+        '\t'
+    ;
+
+    return (x >> (8 * c)) & 0xff;
 }
 
 // I went big endian (highest half nibble first) without thinking much
@@ -40,7 +25,7 @@ half_nibble_to_ws(uint8_t c)
 // for this as would many others :-)
 
 static void
-ws_encode(const uint8_t& c, uint8_t* out)
+encode_one(uint8_t c, uint8_t* out)
 {
     out[0] = half_nibble_to_ws((c >> 6) & 3);
     out[1] = half_nibble_to_ws((c >> 4) & 3);
@@ -48,66 +33,44 @@ ws_encode(const uint8_t& c, uint8_t* out)
     out[3] = half_nibble_to_ws(c & 3);
 }
 
-static void*
-alloc_with_fail(size_t sz, const char* purpose)
-{
-    void* p = malloc(sz);
-    if (!p) {
-        fprintf(
-          stderr, "Failed to allocated '%zu' bytes for %s\n", sz, purpose);
-        perror("Malloc failed");
-        exit(EXIT_FAILURE);
-    }
-
-    return p;
-}
-
 static void
-encode_buffer_interleaved(
-        const uint8_t * const bufin,
-        uint8_t * bufout,
-        const size_t limit
-        )
+encode_buffer_interleaved(const uint8_t* bufin,
+                          uint8_t* bufout,
+                          size_t limit,
+                          size_t num_threads)
 {
-    for (size_t i = 0; i < limit; i += NUM_THREADS) {
-        ws_encode(bufin[i], bufout + 4 * i);
+    for (size_t i = 0; i < limit; i += num_threads) {
+        encode_one(bufin[i], bufout + 4 * i);
     }
 }
 
-static void
-encode_stream(FILE* fin, FILE* fout)
+void
+encode_stream(std::FILE* in, std::FILE* out, size_t num_threads)
 {
-    uint8_t* bufin = (uint8_t*)alloc_with_fail(DECODED_BUFSIZE, "input buffer");
+    uint8_t* bufin = new uint8_t[DECODED_BUFSIZE];
+    uint8_t* bufout = new uint8_t[ENCODED_BUFSIZE];
 
-    uint8_t* bufout =
-      (uint8_t*)alloc_with_fail(ENCODED_BUFSIZE, "output buffer");
-
-    std::thread encoders[NUM_THREADS];
-
+    std::vector<std::thread> encoders(num_threads);
     size_t n;
 
-    while ((n = fread(bufin, 1, DECODED_BUFSIZE, fin))) {
-        for (size_t i = 0; i < NUM_THREADS; ++i) {
-            encoders[i] = std::thread(
-              encode_buffer_interleaved, bufin + i, bufout + 4 * i, n);
+    while ((n = fread(bufin, 1, DECODED_BUFSIZE, in))) {
+        for (size_t i = 0; i < num_threads; ++i) {
+            encoders[i] = std::thread(encode_buffer_interleaved,
+                                      bufin + i,
+                                      bufout + 4 * i,
+                                      n,
+                                      num_threads);
         }
 
         for (auto& encoder : encoders) {
             encoder.join();
         }
 
-        if (fout) {
-            fwrite(bufout, 1, 4 * n, fout);
-        }
+        std::fwrite(bufout, 1, 4 * n, out);
     }
-}
 
-int
-main(void)
-{
-#ifdef _WIN32
-    _setmode(fileno(stdin), O_BINARY);
-    _setmode(fileno(stdout), O_BINARY);
-#endif
-    encode_stream(stdin, stdout);
+    std::fflush(out);
+
+    delete[] bufout;
+    delete[] bufin;
 }
