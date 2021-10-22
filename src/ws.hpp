b9ee1184353d6edea72ff58d560717bc3fed1a40
A. Sinan Unur <sinan@unur.com>
2021-10-22 11:44:43 -0400

    Use simple `#define` for `NUM_THREADS_VAR`

--- src/ws.hpp
+++ src/ws.hpp
@@ -6,7 +6,8 @@ constexpr uint8_t BASE_CHR = static_cast<uint8_t>('\t');
 constexpr size_t DECODED_BUFSIZE = 1 * 1024 * 1024;
 constexpr size_t ENCODED_BUFSIZE = 4 * DECODED_BUFSIZE;
 constexpr size_t NUM_THREADS_DEFAULT = 2;
-constexpr const char* NUM_THREADS_VAR = "WS_NUM_THREADS";
+
+#define NUM_THREADS_VAR "WS_NUM_THREADS"
 
 void(WS_OPERATION)(FILE*, FILE*, size_t);
 
